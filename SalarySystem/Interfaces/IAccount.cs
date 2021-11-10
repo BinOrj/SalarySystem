736a87c24e928406a1c06b6a5e66e408d896e8e9
Binett <73723725+Binett@users.noreply.github.com>
2021-11-10 15:12:56 +0100

    Implemented IAccount

--- SalarySystem/Interfaces/IAccount.cs
+++ SalarySystem/Interfaces/IAccount.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SalarySystem.Models;
 
 namespace SalarySystem.Interfaces
 {
-    interface IAccount
+    public interface IAccount
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public decimal Salary { get; set; }
+        public Roles Role { get; set; }
     }
 }

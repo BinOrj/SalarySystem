f59d1e08c3cf357d437defde937ec0389dc5a4e7
Binett <73723725+Binett@users.noreply.github.com>
2021-11-10 11:10:48 +0100

    Test pull request (#4)
    * Changes in workflow
    
    * Update dotnet.yml
    
    * More changes to main
    
    * Update dotnet.yml
    
    * Broken test for scientific research
    
    * Fixed test, resolves #3
    
    * Team Test
    
    Co-authored-by: Emil Örjes <74154767+emilorjes@users.noreply.github.com>

--- SalarySystem/Program.cs
+++ SalarySystem/Program.cs
@@ -6,7 +6,7 @@ namespace SalarySystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello BinÖrj!");
         }
     }
 }

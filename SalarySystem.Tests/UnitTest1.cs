8677301313c9007480d4f10ff8fc43dc6f0e38d2
Binett <73723725+Binett@users.noreply.github.com>
2021-11-10 09:37:19 +0100

    Add project files.

---
+++ SalarySystem.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+
+namespace SalarySystem.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

4983f5711dc8316983530dca2e1532ad595ace48
Emil Örjes <emil.orjes@hotmail.com>
2021-11-12 10:57:31 +0100

    More testing

--- SalarySystem.Tests/UnitTest1.cs
+++ SalarySystem.Tests/UnitTest1.cs
@@ -26,19 +26,49 @@ namespace SalarySystem.Tests
         }
 
         [Test]
-        public void Test2_Demo_Login()
+        public void Test3_Demo_CheckIfEmployeExists()
         {
 
             LoginController logIn = new();
             List<Employees> employeesList = new();
 
-            Employees test = new ("Test", "123", "Emil", "Örjes", 30000, new Roles[] { Roles.Admin, Roles.Programmer });
-            employeesList.Add(test);
+            Employees testEmploye = new("Test", "123", "Emil", "Örjes", 30000, new Roles[] { Roles.Admin, Roles.Programmer });
+            employeesList.Add(testEmploye);
 
-            var employe = logIn.CheckIfEmployeExists("Test", "123", employeesList);
-            var isAdmin = logIn.IsEmployeAdmin(employe);
+            var existingEmploye = logIn.CheckIfEmployeExists("Test", "123", employeesList);
 
-            Assert.AreEqual(employe, test);
+            Assert.AreEqual(existingEmploye, testEmploye);
+        }
+
+        [Test]
+        public void Test4_Demo_IsEmployeAdmin()
+        {
+
+            LoginController logIn = new();
+            List<Employees> employeesList = new();
+
+            Employees testEmploye = new("Test", "123", "Emil", "Örjes", 30000, new Roles[] { Roles.Admin, Roles.Programmer });
+            employeesList.Add(testEmploye);
+
+            var isAdmin = logIn.IsEmployeAdmin(testEmploye);
+
+            Assert.IsTrue(isAdmin);
+        }
+
+        [Test]
+        public void Test5_Demo_Intergration_LogIn()
+        {
+
+            LoginController logIn = new();
+            List<Employees> employeesList = new();
+
+            Employees testEmploye = new ("Test", "123", "Emil", "Örjes", 30000, new Roles[] { Roles.Admin, Roles.Programmer });
+            employeesList.Add(testEmploye);
+
+            var existingEmploye = logIn.CheckIfEmployeExists("Test", "123", employeesList);
+            var isAdmin = logIn.IsEmployeAdmin(existingEmploye);
+
+            Assert.AreEqual(existingEmploye, testEmploye);
             Assert.IsTrue(isAdmin);
         }
     }

aa820f70283ef41d168bc3fff2a708fb676ad556
Binett <73723725+Binett@users.noreply.github.com>
2021-11-17 18:17:14 +0100

    Added testclass input helper

---
+++ SalarySystem.Tests/InputHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using SalarySystem.Models;
+using SalarySystem.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarySystem.Tests
+{    
+    public class InputHelperTests
+    {
+        private EmployeeHelper _empHelper;
+        private List<Employees> _empList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _empHelper = new();
+            _empList = new();
+        }
+
+        [Test]
+        public void InputHelper_InstanciateNewEmployee_ReturnTrue()
+        {
+            
+        }
+    }
+}
